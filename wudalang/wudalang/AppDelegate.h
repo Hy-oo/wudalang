576781b06361acb63f9f6cf4e39dc58cce047aaf
何源 <heyuan@macbook-air.lan>
2018-03-06 22:16:09 +0800

    新建项目

---
+++ wudalang/wudalang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wudalang
+//
+//  Created by 何源 on 2018/3/6.
+//  Copyright © 2018年 何源. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wudalang/wudalang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wudalang
+//
+//  Created by 何源 on 2018/3/6.
+//  Copyright © 2018年 何源. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
